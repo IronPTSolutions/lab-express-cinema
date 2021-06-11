1cac6b3e10362c243ab6ff5de644ee804f396b5e
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-06-11 23:11:37 +0200

    Done

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+const common = require('../controllers/common.controllers')
+const movies = require('../controllers/movies.controllers')
 
 // Routes here
-
+router.get('/home', common.index)
+router.get('/movies', movies.list)
+router.get('/movies/:movieId', movies.detail)
 module.exports = router;
